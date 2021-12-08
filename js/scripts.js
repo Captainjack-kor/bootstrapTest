b0e1384029714b781f610ee0b34a2f06a7bbe2b2
Captainjack-kor <jennystr233@gmail.com>
2021-12-08 14:54:54 +0900

    attribute add

--- js/scripts.js
+++ js/scripts.js
@@ -7,40 +7,57 @@
 
 let text = "내가... 지금... 뭐라고 ..... 했지?";
 let arr = {
-  idx: ["1", "2", "3", "4", "5"],
-  postTitle: ["우왕 ㅋ굳ㅋ", "오징어게임", "안녕", "하이염", "방가방가"],
-  postContents: ["123", "hi", "aaa", "cc", "bb"],
+  idx: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+  postTitle: ["우왕 ㅋ굳ㅋ", "오징어게임", "안녕", "하이염", "방가방가", "텡", "탕탕탕", "탕탕", "apfjd", "hq", "ggg"],
+  postContents: ["123", "hi", "aaa", "cc", "bb", "ee", "gg", "124", "7766", "325", "asdadsa"],
 };
+
+let pagingArr = [];
+let boardCount = document.getElementsByClassName('lastName')[0].childElementCount;
+console.log(boardCount);
 
 document.getElementsByClassName('mainTitle')[0].innerText = text;
 
-var element  = document.getElementById('tr'); // assuming ul exists
-var fragment = document.createDocumentFragment();
+// var element  = document.getElementById('tr'); // assuming ul exists
+// var fragment = document.createDocumentFragment();
 
 // document.getElementsByClassName('testInfo')[0].children;
 let appendTest = document.getElementsByClassName('lastName')[0];
 var trTest = document.createElement('tr');
 
-arr.idx.forEach((el) => {
-  var td = document.createElement('td');
-  td.textContent = el;
+let pageBtn = document.getElementsByClassName('pageBtn')[0];
+let pageNum = 0;
+arr.idx.forEach((el, idx) => {
+  var trTest = document.createElement('tr');
+  var tdNum = document.createElement('td');
+  var tdContents = document.createElement('td');
+  var tdTitle = document.createElement('td');
+ 
+
+  if(idx % 5 === 0) { //버튼 갯수를 결정
+    var pBtn = document.createElement('button');
+    pBtn.className = 'buttonGap'; //^^;;
+    // pBtn.classList.add('buttonGap');
+    pageNum++;
+    pBtn.textContent = pageNum;
+    pageBtn.append(pBtn);
+  }
+
+  tdNum.textContent = el;
+  tdContents.textContent = arr.postTitle[idx];
+  tdTitle.textContent = arr.postContents[idx];
+
   // fragment.appendChild(td);
-  appendTest.append(td);
+  trTest.append(tdNum);
+  trTest.append(tdContents);
+  trTest.append(tdTitle);
+  appendTest.append(trTest);
 })
 
-arr.postTitle.forEach((el) => {
-  var td = document.createElement('td');
-  td.textContent = el;
-  // appendTest.appendChild(td);
-})
 
-arr.postContents.forEach((el) => {
-  var td = document.createElement('td');
-  td.textContent = el;
-  // fragment.appendChild(td);
-})
+// if(boardCount % 5)
 
-element.appendChild(fragment);
+// element.appendChild(fragment);
 
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
