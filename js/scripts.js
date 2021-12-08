eae47f45af18061f840ead7b9b28e9a61cc17661
Captainjack-kor <jennystr233@gmail.com>
2021-12-08 16:04:51 +0900

    test

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,49 @@
 */
 
 
-let text = "내가... 지금... 뭐라고 ..... 했지?";
+let text = "link move test";
 let arr = {
   idx: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
   postTitle: ["우왕 ㅋ굳ㅋ", "오징어게임", "안녕", "하이염", "방가방가", "텡", "탕탕탕", "탕탕", "apfjd", "hq", "ggg"],
   postContents: ["123", "hi", "aaa", "cc", "bb", "ee", "gg", "124", "7766", "325", "asdadsa"],
 };
 
-let pagingArr = [];
+// 애초에 올 때 부터 짤라야 하나
+
 let boardCount = document.getElementsByClassName('lastName')[0].childElementCount;
 console.log(boardCount);
 
 document.getElementsByClassName('mainTitle')[0].innerText = text;
+
+
+
+function pageAlgo(total, bottomSize, listSize, cursor ){
+  //total = 총 갯수
+  //bottomSize = 하단크기
+  //listSize = 화면에서 보여줄 크기
+  //cursor = 현재 나의 페이지
+
+  let totalPageSize = Math.ceil(total / listSize)  //한 화면에 보여줄 갯수에서 구한 하단 총 갯수 
+
+  let firstBottomNumber = cursor - cursor % bottomSize + 1;  //하단 최초 숫자
+  let lastBottomNumber = cursor - cursor % bottomSize + bottomSize;  //하단 마지막 숫자
+
+  if(lastBottomNumber > totalPageSize) lastBottomNumber = totalPageSize  //총 갯수보다 큰 경우 방지
+
+  return {
+      firstBottomNumber,
+      lastBottomNumber,
+      totalPageSize,
+      total,
+      bottomSize,
+      listSize,
+      cursor
+  }
+}
+
+//280개의 데이터, 하단에는 20개씩, 1개화면에는 10개, 지금 나의페이지는 21
+let info = pageAlgo(boardCount, 5, 5, 11);
+console.log(info);
 
 // var element  = document.getElementById('tr'); // assuming ul exists
 // var fragment = document.createDocumentFragment();
@@ -27,17 +58,36 @@ var trTest = document.createElement('tr');
 
 let pageBtn = document.getElementsByClassName('pageBtn')[0];
 let pageNum = 0;
+
+let firstMoveBtn = document.createElement('button');
+firstMoveBtn.textContent = "<<";
+let leftMoveBtn = document.createElement('button');
+leftMoveBtn.textContent = "<";
+let rightMoveBtn = document.createElement('button');
+rightMoveBtn.textContent = ">";
+let lastMoveBtn = document.createElement('button');
+lastMoveBtn.textContent = ">>";
+
+pageBtn.append(firstMoveBtn);
+pageBtn.append(leftMoveBtn);
+
+let pagingArr = [];
+
 arr.idx.forEach((el, idx) => {
   var trTest = document.createElement('tr');
   var tdNum = document.createElement('td');
   var tdContents = document.createElement('td');
   var tdTitle = document.createElement('td');
- 
-
+  
+  pagingArr.push(el , arr.postTitle[idx],  arr.postContents[idx]);
   if(idx % 5 === 0) { //버튼 갯수를 결정
     var pBtn = document.createElement('button');
     pBtn.className = 'buttonGap'; //^^;;
-    // pBtn.classList.add('buttonGap');
+    pBtn.onclick = function(){
+      
+    };
+    // pBtn.className = 'buttonClick'; //^^;;
+    pBtn.classList.add('buttonClick');
     pageNum++;
     pBtn.textContent = pageNum;
     pageBtn.append(pBtn);
@@ -52,7 +102,26 @@ arr.idx.forEach((el, idx) => {
   trTest.append(tdContents);
   trTest.append(tdTitle);
   appendTest.append(trTest);
+
+  // tdNum.textContent = el;
+  // tdContents.textContent = arr.postTitle[idx];
+  // tdTitle.textContent = arr.postContents[idx];
+
+  // fragment.appendChild(td);
+  trTest.append(tdNum);
+  trTest.append(tdContents);
+  trTest.append(tdTitle);
+
+  appendTest.append(trTest);
+  
 })
+
+pageBtn.append(rightMoveBtn);
+pageBtn.append(lastMoveBtn);
+
+
+console.log(pagingArr);
+
 
 
 // if(boardCount % 5)
